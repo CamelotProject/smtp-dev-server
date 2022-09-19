8eb6528ffe80e8f970d656d333aba2bee22048d3
Gwendolen Lynch <gwendolen.lynch@gmail.com>
2022-09-19 21:30:00 +0200

    Update PHPUnit extension

--- src/PHPUnit/PHPUnitExtension.php
+++ src/PHPUnit/PHPUnitExtension.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Camelot\SmtpDevServer\PHPUnit;
 
 use Camelot\SmtpDevServer\Server;
+use Camelot\SmtpDevServer\Socket\ClientFactory;
+use Camelot\SmtpDevServer\Socket\SmtpSocket;
 use PHPUnit\Runner\AfterLastTestHook;
 use PHPUnit\Runner\BeforeFirstTestHook;
+use Symfony\Component\Console\Output\NullOutput;
 
 class PHPUnitExtension implements BeforeFirstTestHook, AfterLastTestHook
 {
@@ -15,7 +18,8 @@ class PHPUnitExtension implements BeforeFirstTestHook, AfterLastTestHook
     public function executeBeforeFirstTest(): void
     {
         $this->server = new Server();
-        $this->server->start();
+        $this->server->start(new ClientFactory('localhost', 2525, SmtpSocket::class), new NullOutput());
+        $this->server->listen();
     }
 
     public function executeAfterLastTest(): void
